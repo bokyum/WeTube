a5b48f4b10c0302c4ba549506d3a9df197a46b8b
김보겸 <bokyum@gimbogyeom-ui-MacBookPro.local>
2020-07-18 02:25:32 +0900

    to locals

--- app.js
+++ app.js
@@ -8,21 +8,25 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 // require node module을 어딘가에서 가져옴
 const app = express();
-
+app.use(helmet());
 app.set('view engine', "pug")
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(helmet());
+
+app.use(localsMiddleware)
 
 app.use(routes.home, globalRouter);
 app.use(routes.videos, videoRouter);
 app.use(routes.users, userRouter);
 //app.use(routes.home, videoRouter);
+
+
 
 export default app;
 
